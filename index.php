d92b70fbc3fd11f19b0bb9f2318bf90901d6fc84
bitbonsai <m@bitbonsai.com>
2011-09-11 18:09:58 -0300

    Initial commit, still some cleanup to do, this was a couple hours project

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Mauricio Wolff - aka bitbonsai - UXDesigner, WebDesigner,  WebDeveloper</title>
+	<meta name="description" content="Personal space of Mauricio Wolff (bitbonsai). Social Media Links, Github projects, and some other stuff...">
+	<meta name="author" content="Mauricio Wolff">
+
+	<link href="styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div id="masthead">
+		<div class="inner">
+			<div class="container">
+				<h1>Maurício Wolff</h1>
+				<p class="lead">
+					UXDesigner, WebDesigner,  WebDeveloper, A/B Tests, CSS, JS, jQuery, HTML...
+				</p>
+				<p>Well, yet another geek, but a nice one. Works at Dell.com as Web Design lead and Innovator &mdash; Test &amp; Target Team, inside Global Online.</p>
+			</div><!-- /container -->
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="span-two-thirds">
+				<h2>Myself elsewhere</h2>
+				<ul class="tabs">
+					<li class="tab active">
+						<a href="#linkedin">LinkedIn</a>
+					</li>
+					<li class="tab">
+						<a href="#twitter">Twitter (bitbonsai) </a>
+					</li>
+					<li class="tab">
+						<a href="#twittermw">Twitter (mw74) </a>
+					</li>
+					<!-- <li class="tab">
+						<a href="#google">Google+ </a>
+					</li> -->
+					<li class="tab">
+						<a href="#github">GitHub</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle">Other</a>
+						<ul class="dropdown-menu">
+							<li><a target="_blank" href="http://bitbonsai.com">BitBonsai's Blog</a></li>
+							<li><a target="_blank" href="https://plus.google.com/101553334435926941501">Google+</a></li>
+							<li><a target="_blank" href="http://www.facebook.com/mauricio.wolff">Facebook</a></li>
+							<li><a target="_blank" href="http://grooveshark.com/#/mauricio.wolff">Grooveshark</a></li>
+							<li><a target="_blank" href="http://vimeo.com/bitbonsai">Vimeo</a></li>
+							<li><a target="_blank" href="http://webscripts.softpedia.com/developer/Mauricio-Wolff-9531.html">Softpedia</a></li>
+							<li><a target="_blank" href="http://www.xing.com/profile/Mauricio_Wolff">Xing</a></li>
+						</ul>
+					</li>
+				</ul>
+
+				<div class="tabContent on" id="linkedin">
+					<h3 class="muted">Summary</h3>
+					<p>
+						<ul>
+							<li>Results and details-oriented professional with more than 15 years on the craft.</li>
+							<li>Experience in Graphic Design, Interface Design, Information Architecture and Web Development .</li>
+							<li>Highly proficient in web technologies (jQuery, CSS and (x)HTML) as well as design.</li>
+							<li>Innovation oriented, experience with both backend and front end web development.</li>
+							<li>Extremely performance oriented.</li>
+							<li>Strong communicator fluent in English and Portuguese.</li>
+							<li>Leadership and training skills, community driven actions.</li>
+							<li>Highly perceptive, with proven ability to pinpoint problems, provide creative solutions, and follow through to resolution in a timely and cost effective manner.... (<a href="http://linkedin.com/in/mauriciowolff" target="_blank">more</a>)</li>
+						</ul>
+					</p>
+					<a href="http://linkedin.com/in/mauriciowolff" >
+						<img src="http://www.linkedin.com/img/webpromo/btn_myprofile_160x33.png" width="160" height="33" border="0" alt="View Mauricio Wolff's profile on LinkedIn">
+					</a>
+				</div>
+				<div class="tabContent" id="twitter">
+					<div class="follow">
+						<iframe allowtransparency="true" frameborder="0" scrolling="no"
+						  src="http://platform.twitter.com/widgets/follow_button.html?screen_name=bitbonsai&show_count=false"></iframe>
+					</div>
+				</div>
+				<div class="tabContent" id="twittermw">
+					<div class="follow">
+						<iframe allowtransparency="true" frameborder="0" scrolling="no"
+						  src="http://platform.twitter.com/widgets/follow_button.html?screen_name=mw74&show_count=false"></iframe>
+					</div>
+				</div>
+				<div class="tabContent" id="github">
+				</div>
+			</div>
+			<div id="skills" class="span-one-third">
+				<h3>Skills</h3>
+				<h6>FrontEnd Development</h6>
+				<p>HTML, CSS, JS, jQuery, jQueryUI, jQUeryMobile, SVN, GIT, Mercurial, TFS, Photoshop, Fireworks, Illustrator, OmniGraffle</p>
+				
+				<h6>BackEnd Development</h6>
+				<p>PHP, Python, Perl, MySQL, mongoDB</p>
+				
+				<h6>OS</h6>
+				<p>MacOS, Windows, Linux (Ubuntu, CentOS, Gentoo, SuSe), FreeBSD</p>
+				
+				<h6>Things on my radar</h6>
+				<p>Node.js, couchDB, Cucumber, Capybara, Ruby, jqUnit, OOCSS, mobile JS Frameworks</p>
+				<p></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span-one-third">
+				<h3>Resume</h3>
+				<p>If you're feeling like reading two pages of plain text, feel free to <a href="mauriciowolff-resume.pdf">download my PDF resume</a>. Or check more info on <a href="http://linkedin.com/in/mauriciowolff">Linkedin</a></p>
+				<p><a class="btn info" href="mauriciowolff-resume.pdf">Download PDF Resume</a></p>
+			</div>
+			<div class="span-one-third">
+				<h3>Projects</h3>
+				<p>I have a couple projects going on, like jQuery plugins (<a href="https://github.com/bitbonsai/facybox" target="_blank">Facybox</a>, <a href="https://github.com/bitbonsai/waitill" target="_blank">Waitill</a> &mdash; check my <a href="https://github.com/bitbonsai">github account</a> or <a href="http://abpedia.org">abpedia.org</a>, a site about AB Tests in Portuguese, a joint venture with <a href="http://cristianosantos.com.br/" target="_blank">Cristiano Santos</a> and <a href="http://tiagosartor.com" target="_blank">Tiago Sartor</a>.</p>
+				<p>
+					<a class="btn" target="_blank" href="http://github.com/bitbonsai">My GitHub account &raquo;</a>
+					<a class="btn" target="_blank" href="http://abpedia.org">See abpedia.org &raquo;</a>
+				</p>
+			</div>
+			<div class="span-one-third">
+				<h3>About This little site</h3>
+				<p>This is my first experiment with <a href="https://github.com/twitter/bootstrap" target="_blank">BootStrap</a>, a CSS framework from Twitter. I also used a stripped down version of <a href="http://html5boilerplate.com/" target="_blank">HTML5 boilerplate</a> as well as <a href="http://code.google.com/closure/" target="_blank">Google Closure Compiler</a> to compress the  JS/CSS. <a href="https://github.com/bitbonsai/mauriciowolff.com">All this code is on github</a>, so fell free to clone and twist it. Please make a pull request so I can learn from that too...</p>
+			</div>
+		</div>
+	</div> <!-- /container -->
+
+	<div id="footer">
+		<div class="inner">
+			<div class="container">
+				<p class="right"><a href="#">Go to top</a></p>
+				<p>
+					Designed and built with all the love by <a href="http://twitter.com/bitbonsai" target="_blank">@bitbonsai</a>. <span class="muted">BootStrap's footer rocks.</span><br>
+					Mauricio Wolff doesn't care about attribution at all. — It's all yours under a <a target="_blank" href="http://creativecommons.org/publicdomain/zero/1.0/">Creative Commons 0</a> license.<br>
+				</p>
+			</div>
+		</div>
+	</div> <!-- footer -->
+
+	<script src="http://code.jquery.com/jquery-1.6.3.min.js" type="text/javascript"></script>
+	<script src="init.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-25654290-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+</body>
+</html>
